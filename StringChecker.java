3da004c458cda7492e344e92f15bd8de7e97f6d1
agordov <agordov96@gmail.com>
2017-04-17 16:41:15 +0300

    Some methods from InputChecker replaced into StringChecker

---
+++ StringChecker.java
@@ -0,0 +1,32 @@
+package javaFx;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class StringChecker {
+
+    private String line;
+    private static final String SEPARATOR = " ";
+
+    StringChecker(String str) {
+        this.line = str;
+    }
+
+    boolean containSymbols(String symbols) {
+        boolean flag = line.matches(String.format(".*%s.*", symbols));
+        if (flag) {
+            throw new RuntimeException("Contains symbols: " + getSymbols(symbols));
+        }
+        return false;
+    }
+
+    private String getSymbols(String symbols) {
+        Pattern p = Pattern.compile(symbols);
+        Matcher m = p.matcher(line);
+        String matched = "";
+        while (m.find()) {
+            matched += (m.group() + SEPARATOR);
+        }
+        return matched;
+    }
+}
